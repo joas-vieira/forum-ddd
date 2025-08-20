f6242b4726f2103847030428cbc16b1307945dcc
joas-vieira <joas.vieira.dev@gmail.com>
2025-08-20 10:12:17 -0300

    feat: add answer question use case

---
+++ src/domain/use-cases/answer-question.use-case.ts
@@ -0,0 +1,28 @@
+import { Answer } from "../entities/answer.entity.js";
+import type { AnswerRepository } from "../repositories/answer.repository.js";
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private readonly answerRepository: AnswerRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      authorId: instructorId,
+      questionId,
+      content,
+    });
+
+    await this.answerRepository.create(answer);
+
+    return answer;
+  }
+}
---
+++ src/domain/use-cases/tests/answer-question.use-case.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "../answer-question.use-case.js";
+import type { AnswerRepository } from "../../repositories/answer.repository.js";
+
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer) => {},
+};
+
+test("create an answer", async () => {
+  const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await answerQuestionUseCase.execute({
+    instructorId: "1",
+    questionId: "1",
+    content: "This is an answer",
+  });
+
+  expect(answer.content).toEqual("This is an answer");
+});
